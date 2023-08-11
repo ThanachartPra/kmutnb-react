05b6f798a0387e3d7a02179b21c5441fea5d1fe3
KhunKittys <110073990+ThanachartPra@users.noreply.github.com>
2023-08-11 15:43:53 +0700

    Create props

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import {Header, Content, Footer} from './func-components'
 import Calendar from "./class-components";
+import MsgBox from "./class-props";
 
 function App() {
   return (
@@ -9,8 +10,15 @@ function App() {
     <p><center><Calendar/></center></p>
     <Content/>
     <Footer/>
+    <MsgBox
+      text = "React & React Native"
+      color = "blue"
+      bgColor = "#ccc"
+      fontSize = "18pt"
+      border = "solid 1px black"
+    />
     </>
-    
+
   )
 }
 
