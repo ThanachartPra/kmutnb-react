b18f8667ad26123bbdc713d8642ef8c7b4c0332d
ThanachartPra <110073990+ThanachartPra@users.noreply.github.com>
2023-08-25 14:44:01 +0700

    add refs-array.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
 import {Header, Content, Footer} from './func-components'
 import Calendar from "./class-components";
+import RefsArray from "./refs-array";
+/*
 import RefsClass from "./refs-class";
 import RefsFunc from "./refs-func";
-/*
 import MsgBox from "./class-props";
 import Banner from "./banner";
 import { Calculator } from "./calculator";
@@ -18,8 +19,7 @@ function App() {
     <Header/>
     <p><center><Calendar/></center></p>
     <Content/>
-    <RefsClass/>
-    <RefsFunc/>
+    <RefsArray/>
     <Footer/>
     </>
   )
