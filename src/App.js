2b112a2ff1b4e825131cdbbd85838c8de255ccff
ThanachartPra <110073990+ThanachartPra@users.noreply.github.com>
2023-08-25 14:04:45 +0700

    add refs-class,refs-func.js

--- src/App.js
+++ src/App.js
@@ -1,34 +1,27 @@
 import React from "react";
 import {Header, Content, Footer} from './func-components'
 import Calendar from "./class-components";
+import RefsClass from "./refs-class";
+import RefsFunc from "./refs-func";
+/*
 import MsgBox from "./class-props";
 import Banner from "./banner";
 import { Calculator } from "./calculator";
 import { Button } from "./class-components";
 import { EventData } from "./event-data";
 import { Table } from "./event-data";
+*/
 
 function App() {
   return (
     <>
-    <Banner/>
     <Header/>
     <p><center><Calendar/></center></p>
     <Content/>
-    <Button/>
-    <Calculator/>
-    <EventData/>
-    <Table/>
+    <RefsClass/>
+    <RefsFunc/>
     <Footer/>
-    <MsgBox
-      text = "React & React Native"
-      color = "blue"
-      bgColor = "#ccc"
-      fontSize = "18pt"
-      border = "solid 1px black"
-    />
     </>
-
   )
 }
 
